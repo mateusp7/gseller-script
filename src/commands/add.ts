359198053381ebb120bb6b73aaee473142bf61fc
mateusp7 <mateusdev1337@gmail.com>
2024-01-05 12:43:36 -0300

    Feat: remove src in gitignore

---
+++ src/commands/add.ts
@@ -0,0 +1,91 @@
+import chalk from 'chalk'
+import { Command } from 'commander'
+import { existsSync } from 'fs'
+import path from 'path'
+import prompts from 'prompts'
+import { z } from 'zod'
+// import { getConfig } from '../helpers/get-config'
+import { logger } from '../helpers/logger'
+
+const componentsList = [
+  {
+    title: 'Login',
+    value: 'login',
+  },
+  {
+    title: 'Administradores',
+    value: 'administrators',
+  },
+]
+
+const addOptionsSchema = z.object({
+  components: z.array(z.string()).optional(),
+  yes: z.boolean(),
+  cwd: z.string(),
+})
+
+export const add = new Command()
+  .name('add')
+  .description('Adicionar uma funcionalidade/componente para o projeto Gseller')
+  .argument('[components...]', 'componente que irá ser adicionado')
+  .option('-y, --yes', 'pular a confirmação de criação do projeto', true)
+  .option(
+    '-c, --cwd <cwd>',
+    'Diretório de trabalho. O padrão é o diretório atual.',
+    process.cwd()
+  )
+  .action(async (components, opts) => {
+    console.log('opts', opts)
+    console.log('components', components)
+    try {
+      const options = addOptionsSchema.parse({
+        components,
+        ...opts,
+      })
+
+      const cwd = path.resolve(options.cwd)
+
+      if (!existsSync(cwd)) {
+        logger.error(`O caminho ${cwd} não existe. Por favor, tente novamente.`)
+        process.exit(1)
+      }
+
+      await promptQuestions(options)
+    } catch (err) {
+      console.log('error', err)
+    }
+  })
+
+export async function promptQuestions(
+  options: z.infer<typeof addOptionsSchema>
+) {
+  const cwd = path.resolve(options.cwd)
+  const highlight = (text: string) => chalk.cyan(text)
+  let selectedComponents = options.components
+
+  if (!options.components?.length) {
+    const { components } = await prompts({
+      type: 'multiselect',
+      name: 'components',
+      message: 'Qual componente você gostaria de adicionar?',
+      hint: 'ESPAÇO para selecionar. A para marcar todos. Enter para enviar',
+      instructions: false,
+      choices: componentsList.map((entry) => ({
+        title: entry.title,
+        value: entry.value,
+        selected: options.components?.includes(entry.value),
+      })),
+    })
+    selectedComponents = components
+  }
+
+  if (!selectedComponents?.length) {
+    logger.warn('Nenhum componente selecionado. Saindo.')
+    process.exit(0)
+  }
+
+  // const config = await getConfig(cwd)
+  // console.log('config', config)
+
+  // const tree = await resolveTree(registryIndex, selectedComponents)
+}
---
+++ src/commands/init.ts
@@ -0,0 +1,144 @@
+import { Command } from 'commander'
+import { existsSync, promises as fs } from 'fs'
+import ora from 'ora'
+import path from 'path'
+
+import chalk from 'chalk'
+import prompts from 'prompts'
+import { z } from 'zod'
+import { createApp } from '../helpers/create-app'
+import {
+  Config,
+  DEFAULT_COMPONENTS,
+  DEFAULT_GRAPHQL,
+  DEFAULT_TAILWIND_CONFIG,
+  DEFAULT_TAILWIND_CSS,
+  DEFAULT_UTILS,
+  getConfig,
+  rawConfigSchema,
+} from '../helpers/get-config'
+import { logger } from '../helpers/logger'
+import { makeDir } from '../helpers/make-dir'
+
+const initOptionsSchema = z.object({
+  cwd: z.string(),
+  yes: z.boolean(),
+})
+
+
+
+export const init = new Command()
+  .command('init')
+  .description('Iniciar projeto boilerplate Gseller')
+  .option('-y, --yes', 'pular a confirmação de criação do projeto', false)
+  .option(
+    '-c, --cwd <cwd>',
+    'Diretório de trabalho. O padrão é o diretório atual.',
+    process.cwd()
+  )
+  .action(async (opts) => {
+    const example = 'https://github.com/mateusp7/grv'
+
+    try {
+      const options = initOptionsSchema.parse(opts)
+      const cwd = path.resolve(options.cwd)
+
+      if (!existsSync(cwd)) {
+        logger.error(`O caminho ${cwd} não existe. Por favor, tente novamente.`)
+        process.exit(1)
+      }
+
+      const existingConfig = await getConfig(cwd)
+      const prompt = await promptForConfig(existingConfig)
+
+      // await runInit(prompt, example)
+
+      logger.info('')
+      logger.info(`Inicialização do projeto realizada com sucesso.`)
+      logger.info('')
+    } catch (err) {}
+  })
+
+export async function promptForConfig(defaultConfig: Config | null = null) {
+  const highlight = (text: string) => chalk.cyan(text)
+
+  const options = await prompts([
+    {
+      type: 'text',
+      name: 'graphql',
+      message: `Aonde está localizado a pasta ${highlight('graphql')}?`,
+      initial: defaultConfig?.graphql ?? DEFAULT_GRAPHQL,
+    },
+    {
+      type: 'text',
+      name: 'tailwindConfig',
+      message: `Aonde está localizado a pasta ${highlight(
+        'tailwind.config.js'
+      )}?`,
+      initial: defaultConfig?.tailwind.config ?? DEFAULT_TAILWIND_CONFIG,
+    },
+    {
+      type: 'text',
+      name: 'tailwindCss',
+      message: `Aonde está localizado o arquivo ${highlight('global CSS')}?`,
+      initial: defaultConfig?.tailwind.css ?? DEFAULT_TAILWIND_CSS,
+    },
+    {
+      type: 'text',
+      name: 'components',
+      message: `Configure o alias de importação para ${highlight(
+        'components'
+      )}:`,
+      initial: defaultConfig?.aliases['components'] ?? DEFAULT_COMPONENTS,
+    },
+    {
+      type: 'text',
+      name: 'utils',
+      message: `Configure o alias de importação para ${highlight('utils')}:`,
+      initial: defaultConfig?.aliases['utils'] ?? DEFAULT_UTILS,
+    },
+  ])
+
+  const config = rawConfigSchema.parse({
+    tsx: true,
+    graphql: options.graphql,
+    tailwind: {
+      config: options.tailwindConfig,
+      css: options.tailwindCss,
+    },
+    aliases: {
+      utils: options.utils,
+      components: options.components,
+    },
+  })
+
+  const resolvedProjectPath = path.resolve()
+  logger.info('')
+
+  createGsellerJson(resolvedProjectPath, config)
+
+  return resolvedProjectPath
+}
+
+export async function createGsellerJson(
+  cwd: string,
+  config: z.infer<typeof rawConfigSchema>
+) {
+  logger.info('')
+  const spinner = ora(`Criando arquivo gseller.json...`).start()
+
+  const targetPath = path.resolve(cwd, 'gseller.json')
+  await fs.writeFile(targetPath, JSON.stringify(config, null, 2), 'utf-8')
+  spinner.succeed()
+}
+
+export async function runInit(cwd: string, example: string) {
+  // await addNextGlobal()
+  // await startNextProject(cwd)
+  try {
+    await createApp({
+      appPath: cwd,
+      example: example,
+    })
+  } catch (reason) {}
+}
